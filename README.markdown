9f9c630fb6d26d8462c659454de4a00f95659cab
Navid <navid.tavalaei@gmail.com>
2020-04-03 18:57:48 +0200

    first commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Full Page Parallax Scroll Effect
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/navidtavalaei/pen/dyoEVOV](https://codepen.io/navidtavalaei/pen/dyoEVOV).
+
+ A lightweight full page parallax scroll effect.
---
+++ dist/script.js
@@ -0,0 +1,96 @@
+// ------------- VARIABLES ------------- //
+var ticking = false;
+var isFirefox = (/Firefox/i.test(navigator.userAgent));
+var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
+var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
+var slideDurationSetting = 600; //Amount of time for which slide is "locked"
+var currentSlideNumber = 0;
+var totalSlideNumber = $(".background").length;
+
+// ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
+function parallaxScroll(evt) {
+  if (isFirefox) {
+    //Set delta for Firefox
+    delta = evt.detail * (-120);
+  } else if (isIe) {
+    //Set delta for IE
+    delta = -evt.deltaY;
+  } else {
+    //Set delta for all other browsers
+    delta = evt.wheelDelta;
+  }
+
+  if (ticking != true) {
+    if (delta <= -scrollSensitivitySetting) {
+      //Down scroll
+      ticking = true;
+      if (currentSlideNumber !== totalSlideNumber - 1) {
+        currentSlideNumber++;
+        nextItem();
+      }
+      slideDurationTimeout(slideDurationSetting);
+    }
+    if (delta >= scrollSensitivitySetting) {
+      //Up scroll
+      ticking = true;
+      if (currentSlideNumber !== 0) {
+        currentSlideNumber--;
+      }
+      previousItem();
+      slideDurationTimeout(slideDurationSetting);
+    }
+  }
+}
+
+// ------------- SET TIMEOUT TO TEMPORARILY "LOCK" SLIDES ------------- //
+function slideDurationTimeout(slideDuration) {
+  setTimeout(function() {
+    ticking = false;
+  }, slideDuration);
+}
+
+// ------------- ADD EVENT LISTENER ------------- //
+var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
+
+// ------------- SLIDE MOTION ------------- //
+function nextItem() {
+  var $previousSlide = $(".background").eq(currentSlideNumber - 1);
+  $previousSlide.removeClass("up-scroll").addClass("down-scroll");
+}
+
+function previousItem() {
+  var $currentSlide = $(".background").eq(currentSlideNumber);
+  $currentSlide.removeClass("down-scroll").addClass("up-scroll");
+}
+(() => {
+  // Specify the deadline date
+  const deadlineDate = new Date('July 02, 2020 23:59:59').getTime();
+  
+  // Cache all countdown boxes into consts
+  const countdownDays = document.querySelector('.countdown__days .number');
+  const countdownHours= document.querySelector('.countdown__hours .number');
+  const countdownMinutes= document.querySelector('.countdown__minutes .number');
+  const countdownSeconds= document.querySelector('.countdown__seconds .number');
+
+  // Update the count down every 1 second (1000 milliseconds)
+  setInterval(() => {    
+    // Get current date and time
+    const currentDate = new Date().getTime();
+
+    // Calculate the distance between current date and time and the deadline date and time
+    const distance = deadlineDate - currentDate;
+
+    // Calculations the data for remaining days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 61 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Insert the result data into individual countdown boxes
+    countdownDays.innerHTML = days;
+    countdownHours.innerHTML = hours;
+    countdownMinutes.innerHTML = minutes;
+    countdownSeconds.innerHTML = seconds;
+  }, 1000);
+})();
